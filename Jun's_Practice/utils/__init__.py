3e787c53ccbcfde0ef6dc0527fa448edc527dacf
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-01-02 13:31:47 +0900

    feat : im2col, col2im 메서드 추가 구현

--- Jun's_Practice/utils/__init__.py
+++ Jun's_Practice/utils/__init__.py
@@ -1,5 +1,5 @@
 # utils/__init__.py
 
-from .functions import softmax, cross_entropy_error  # 필요한 함수만 가져오기
+from .functions import softmax, cross_entropy_error, im2col, col2im  # 필요한 함수만 가져오기
 
-__all__ = ['softmax', 'cross_entropy_error']  # 외부에서 접근 가능한 함수 명시
+__all__ = ['softmax', 'cross_entropy_error', 'im2col', 'col2im']  # 외부에서 접근 가능한 함수 명시
